647f87fbb650ecd5b98cf719d0444ee799dbe499
Sharaf-TMU-CS <sharafmine227@gmail.com>
2024-09-01 21:40:41 -0400

    Add files via upload

---
+++ CPS 209 Lab 1/Linux/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+   A simple main program that prints Hello World
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+	//-----------Start below here. To do: approximate lines of code = 1
+	//  Write a line the prints "Hello World" without the quotes.
+	System.out.println("Hello World"); // I couldn't get it to work in the lab and I'm not on Linux
+	//-----------------End here. Please do not remove this comment. Reminder: no changes outside the todo regions.
+    }
+}
